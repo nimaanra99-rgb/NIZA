315cc31408ed30f61ac684438316e415eded4237
nimaanra99-rgb <anranima99@gmail.com>
2026-02-07 02:13:25 -0600

    Delete Pag. WEB directory

--- Pag. WEB/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.nav a').forEach(link => {
-    link.addEventListener('click', e => {
-        if (link.getAttribute('href').startsWith('#')) {
-            e.preventDefault();
-            document.querySelector(link.getAttribute('href'))
-                .scrollIntoView({ behavior: 'smooth' });
-        }
-    });
-});
